e5d6447898b944fe013eb71d6241e21092db098a
Mahdi Jaffery <mahdijaffri5@gmail.com>
2025-06-26 12:02:04 +0500

    Initial Commit

---
+++ PathWise/Helper.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def getNeighbours(current, Maze, diagonal = False):
+    neighbours = []
+    cx, cy = current
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)] 
+    
+    newMaze = np.array(Maze)
+
+    if diagonal:
+        directions += [(1, 1), (1, -1), (-1, 1), (-1, -1)]
+    
+    for dx, dy in directions:
+        x, y = cx + dx, cy + dy
+
+        if 0 <= x < len(newMaze) and 0 <= y < len(newMaze[0]) and newMaze[x][y] != '1':
+            neighbours.append((x, y))
+    return neighbours
