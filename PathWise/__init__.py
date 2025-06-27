73961703aeefa96c0ea4e4dea3b2d1d9ce8645f5
Mahdi Jaffery <mahdijaffri5@gmail.com>
2025-06-27 15:22:58 +0500

    Added Maze Generator

--- PathWise/__init__.py
+++ PathWise/__init__.py
@@ -5,10 +5,10 @@ from .DFS import DFS
 from .UCS import UCS
 
 class PathWise:
-    def __init__(self, Maze):
-        self.Maze = Maze
+    def __init__(self):
+        self.Maze = None
         pass
-
+    
     def DFS(self, start, end, diagonal = False):
         return DFS(self.Maze, start, end, diagonal)
 
@@ -29,14 +29,44 @@ class PathWise:
         for i in range(len(newMaze)):
             for j in range(len(newMaze[i])):
                 if newMaze[i][j] == '0' or newMaze[i][j].lower() == 'g' or newMaze[i][j].lower() == 's':
-                    newMaze[i][j] = ' '
+                    if newMaze[i][j] == '0':
+                        newMaze[i][j] = ' '
                 elif newMaze[i][j] == '1':
                     newMaze[i][j] = '#'
                 else:
                     newMaze[i][j] = 'X'
 
         print(np.array(newMaze))
-        print(np.array(self.Maze))
+        pass
+
+    def generateMaze(self, maxRows, maxCols, density = 0.3):
+        self.Maze = np.random.choice(['0', '1'], size=(maxRows, maxCols), p=[1 - density, density])
+        start = (np.random.randint(0, maxRows), np.random.randint(0, maxCols))
+        end = (np.random.randint(0, maxRows), np.random.randint(0, maxCols))
+        self.Maze[start] = 'S'
+        self.Maze[end] = 'G'
+
+        self.start = start
+        self.end = end
+
+        return self.Maze
+
+    def setMaze(self, maze):
+        self.Maze = maze
+        if not np.issubdtype(self.Maze.dtype, np.integer):
+            self.Maze = np.array(self.Maze)
+        start = None
+        end = None
+        maze_arr = np.array(self.Maze)
+        for i in range(maze_arr.shape[0]):
+            for j in range(maze_arr.shape[1]):
+                if maze_arr[i, j] == 'S':
+                    start = (i, j)
+                elif maze_arr[i, j] == 'G':
+                    end = (i, j)
+        self.start = start
+        self.end = end
+        return self.Maze
 
     pass
 PathWise = PathWise
